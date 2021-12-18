0d67bd6350e1868ce76dc486d40e87bcd46b77ac
John Sloboda <sloboda@gmail.com>
2021-12-18 17:32:12 -0500

    works now without pkgs needing to be installed to environment (code is a disaster and needs to be cleaned up)

--- topiary/cli.py
+++ topiary/cli.py
@@ -9,19 +9,20 @@ import pip_api
 import toml
 
 
-def not_stdlib_modules(modname: str) -> bool:
+def not_stdlib_modules(modname: str, site_pkg_mods: List[str]) -> bool:
     """Determines whether or not a module is part of the python standard library.
     """
     print(f'modname: {modname}')
-    try:
-        spec = importlib.util.find_spec(modname)
-        print(f'module spec: {spec}, parent: {spec.parent}')
-        return '/site-packages/' in spec.origin
-    except (AttributeError, ModuleNotFoundError):
-        return True
+    #try:
+    #    spec = importlib.util.find_spec(modname)
+    #    print(f'module spec: {spec}, parent: {spec.parent}')
+    #    return '/site-packages/' in spec.origin
+    #except (AttributeError, ModuleNotFoundError):
+    #    return True
+    return modname in site_pkg_mods
 
 
-def extract_pkg_imports(filepath: str) -> List[str]:
+def extract_pkg_imports(filepath: str, site_pkg_mods: List[str]) -> List[str]:
     """Given a path to a python file, return a list of all modules imported at the top level.
     """
     with open(filepath) as f:
@@ -35,22 +36,36 @@ def extract_pkg_imports(filepath: str) -> List[str]:
             if node.level == 0:
                 mods.append(node.module)
 
-    return [m.split('.')[0] for m in mods if not_stdlib_modules(m)]
+    return [m.split('.')[0] for m in mods if not_stdlib_modules(m.split('.')[0], site_pkg_mods)]
+
+
+def get_venv_dist_pkgs() -> Dict[str, str]:
+    venv_lib_path = os.path.join(os.environ['VIRTUAL_ENV'], 'lib')
+    py_dir = os.listdir(venv_lib_path)[0]
+    site_packages_path = os.path.join(venv_lib_path, py_dir, 'site-packages')
+    dist_info_dirs = [d for d in os.listdir(site_packages_path) if d.endswith('.dist-info')]
+    return {d.split('-', 1)[0]: os.path.join(site_packages_path, d) for d in dist_info_dirs}
 
 
 def make_mod_to_pkg_map(pkgs: Iterable[str]) -> Dict[str, str]:
     pkg_set = set(pkgs)
     pkg_set.remove('python')
-    dists = {name: pkg for name, pkg in pip_api.installed_distributions(local=False).items() if name in pkg_set}
-    dist_set = set(dists.keys())
-    if any(pkg not in dist_set for pkg in pkg_set):
-        raise Exception(f'some pkgs in pyproject.toml are not installed: {pkg_set - dist_set}')
+    #dists = {name: pkg for name, pkg in pip_api.installed_distributions(local=False).items() if name in pkg_set}
+    dists = get_venv_dist_pkgs()
+    print(dists)
+    #dist_set = set(dists.keys())
+    #if any(pkg not in dist_set for pkg in pkg_set):
+    #    raise Exception(f'some pkgs in pyproject.toml are not installed: {pkg_set - dist_set}')
     mod_to_pkg_map = {}
-    for d in dists.values():
-        top_level_filename = f'{d.location}/{d.name.replace("-", "_")}-{d.version}.dist-info/top_level.txt'
-        with open(top_level_filename) as f:
-            top_level_mod = f.read().rstrip('\n')
-            mod_to_pkg_map[top_level_mod] = d.name
+    for name, path in dists.items():
+        #top_level_filename = f'{d.location}/{d.name.replace("-", "_")}-{d.version}.dist-info/top_level.txt'
+        try:
+            top_level_filename = f'{path}/top_level.txt'
+            with open(top_level_filename) as f:
+                top_level_mod = f.read().rstrip('\n')
+                mod_to_pkg_map[top_level_mod] = name
+        except FileNotFoundError:
+            pass
     print(mod_to_pkg_map)
     return mod_to_pkg_map
 
@@ -105,13 +120,14 @@ def main():
     pyproj = toml.load('pyproject.toml')
     pyproj_deps = pyproj['tool']['poetry']['dependencies']
 
-    imported_modules = set(imp for fp in code_filepaths for imp in extract_pkg_imports(fp))
-    print(f'imported modules: {imported_modules}')
 
-    dep_pkgs = pyproj_deps.keys()
+    dep_pkgs = set(pkg.replace('-', '_') for pkg in pyproj_deps.keys())
     print(f'dep pkgs: {dep_pkgs}')
     mod_to_pkg_map = make_mod_to_pkg_map(dep_pkgs)
-    imported_pkgs = [pkg for mod, pkg in mod_to_pkg_map.items() if mod in imported_modules]
+
+    imported_modules = set(imp for fp in code_filepaths for imp in extract_pkg_imports(fp, list(mod_to_pkg_map.keys())))
+    print(f'imported modules: {imported_modules}')
+    imported_pkgs = set(pkg for mod, pkg in mod_to_pkg_map.items() if mod in imported_modules)
     print(f'imp pkgs: {imported_pkgs}')
 
     unnecessary_pkgs = dep_pkgs - imported_pkgs
